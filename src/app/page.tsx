389b009285d10afad6d0dbd85eb0fc4a3c6e247c
sah_314 <khadayatesahil@gmail.com>
2023-10-15 00:07:29 +0530

    Config updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
+import TypewriterTitle from '@/components/TypewriterTitle'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
-
+import Link from 'next/link'
+import { ArrowRight } from 'lucide-react'
 export default function Home() {
   return (
-    <div className=' bg-gradient-to-r min-h-screen from-[#b1d5e2] to-[#ef71ea]'>
+    <div className=' bg-gradient-to-r min-h-screen grainy from-[#b1d5e2] to-[#ef71ea]'>
           <div className='absolute top-1/2 left-1/2 -translate-x-1/4 -translate-y-1/2'>
             <h1 className=' font-medium text-7xl'>
               <span className='font-bold text-[#165e78]'> AI Assisted </span> Notetaking
             </h1>
+            <div className="mt-4"></div>
+            <h2 className="font-medium text-3xl text-slate-500">
+            <TypewriterTitle/>
+            </h2>
+            <div className="mt-4"></div>
+            <Link href='/dashboard'>
+              <Button className=' bg-[#165e78]'> Get Started
+              <ArrowRight className=' ml-2' strokeWidth={3}/>
+              </Button>
+            </Link>
           </div>
 
 

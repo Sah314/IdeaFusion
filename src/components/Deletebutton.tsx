a1feb6fe5e33b3c5532fceb14bf637734f150e06
sah_314 <khadayatesahil@gmail.com>
2023-10-16 20:47:33 +0530

    Added api call to Deletebutton

--- src/components/Deletebutton.tsx
+++ src/components/Deletebutton.tsx
@@ -1,17 +1,39 @@
+'use client'
 import React from 'react'
 import { Button } from './ui/button'
 import { Trash } from 'lucide-react'
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
 type Props = {
     NoteId:number
 }
 
 const Deletebutton = ({NoteId}: Props) => {
+  
+  const router = useRouter()
+  const deleteNote = useMutation({
+      mutationFn:async()=>{
+        const response  = await axios.post('/api/deleteNote',{
+          NoteId
+        })
+        return response.data;
+      }
+  })
   return (
     <Button variant={'destructive'} size='sm' onClick={()=>{
         const confirm = window.confirm("Are you sure you want to delete this note?")
         if(!confirm) return;
-        
+        deleteNote.mutate(undefined,{
+          onSuccess:()=>{
+            console.log("Successfully deleted note");
+            router.push('/dashboard');
+          },
+          onError:(err)=>{
+              console.error(err);
+          }
+        })
     }} >
         <Trash/>
     </Button>

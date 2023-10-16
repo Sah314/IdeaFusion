9663160dfb50263acf700c1465ea9ab2263fb7ec
sah_314 <khadayatesahil@gmail.com>
2023-10-16 20:16:33 +0530

    Added editor to individual note components

--- src/app/notebook/[notesId]/page.tsx
+++ src/app/notebook/[notesId]/page.tsx
@@ -1,10 +1,15 @@
+import { Button } from '@/components/ui/button';
 import { db } from '@/lib';
 import { $Note } from '@/lib/schema';
 import { auth } from '@clerk/nextjs';
 import {and,eq} from 'drizzle-orm'
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
+import { ArrowLeft } from 'lucide-react';
 import { NextResponse } from 'next/server';
 import React from 'react'
+import { clerk } from '@/lib/clerk-server';
+import TiptapEditor from '@/components/TiptapEditor';
 
 type Props = {
     params:{
@@ -18,6 +23,8 @@ const NotebookPage = async({params:{notesId}}: Props) => {
     if(!userId){
         return redirect('/dashboard');  
     }
+const user = await clerk.users.getUser(userId);
+
     const notes = await db.select().from($Note).where(
         and(
             eq($Note.id,parseInt(notesId)),
@@ -28,8 +35,26 @@ const NotebookPage = async({params:{notesId}}: Props) => {
         return redirect('/dashboard');
     }
     const note = notes[0];
+    
   return (
-    <pre >{ JSON.stringify(note,null,2)}</pre>
+    <div className='min-h-screen grainy p-7'>
+        <div className="mx-auto max-w-4xl">
+            <div className="border shadow-lg border-stone-200 rounded-xl p-4 flex items-center">
+                <Link href='/dashboard'>
+                    <Button className="bg-[#7a4593]" size='sm'><ArrowLeft className='mr-2' strokeWidth={3}/> Back</Button>
+                </Link>
+                <div className="w-3"></div>
+                <span className='font-semibold'>{user.firstName} {user.lastName}</span>
+                <span className='inline-block mx-1'>/</span>
+                <div className="text-stone-500 font-semibold">{note.name}</div>
+                <div className="ml-auto">DELETE</div>
+            </div>
+            <div className="h-4"></div>
+            <div className="border-stone-200 shadow-xl border rounded-lg px-14 py-8 w-full">
+                <TiptapEditor note={note}/>
+            </div>
+        </div>
+    </div>
   )
 }
 
